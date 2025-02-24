1d0162533dfe658a7e7082de413a3c8bf1b864ac
HarryCrossman1 <hc1746@my.acm.ac.uk>
2025-02-24 23:06:44 +0000

    save and load worlds
    this took years to do i was bashing my head into the walls for hours

--- EchoesOfTheUnderground/Assets/Scripts/ProceduralGeneration.cs
+++ EchoesOfTheUnderground/Assets/Scripts/ProceduralGeneration.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
+
+[System.Serializable]
+public class WorldData
+{
+    public List<Vector3> BlockPositions = new List<Vector3>();
+    public List<Quaternion> ObjectRotation = new List<Quaternion>();
+    public List<string> ObjectType = new List<string>();
+}
 
 public class ProceduralGeneration : MonoBehaviour
 {
@@ -10,20 +19,37 @@ public class ProceduralGeneration : MonoBehaviour
     [SerializeField] private GameObject Block;
     [SerializeField] private int WorldSizeX;
     [SerializeField] private int WorldSizeZ;
-    [SerializeField] private int NoiseHeight;
     [SerializeField] private float GridSpacing;
-    [Range(1,2)] [SerializeField] private float ObjectDensity;
-    [Range(1, 2)][SerializeField] private float PickupDensity;
+    [SerializeField] [Range(0,100)] public int CarChance;
+    private bool JustGenerated;
     [SerializeField] private GameObject[] PlaceableSceneObjects;
     [SerializeField] private GameObject[] PlaceablePickups;
-    [SerializeField] private bool JustGenerated;
+   [SerializeField] private List<GameObject> spawnedBlocks = new List<GameObject>();
+    private List<GameObject> spawnedObjects = new List<GameObject>();
+    private string saveFilePath;
+    private int Index;
 
     private void Awake()
     {
-        GenerateGrid(WorldSizeX, WorldSizeZ);
-        //NavmeshSurface.BuildNavMesh();
-        GenerateNavmesh();
-       
+        saveFilePath = Path.Combine(Application.persistentDataPath, "worlddata.json");
+        if (File.Exists(saveFilePath))
+        {
+            LoadWorld();
+        }
+        else
+        {
+            GenerateGrid(WorldSizeX, WorldSizeZ);
+           
+            GenerateNavmesh();
+           
+        }  
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        { 
+            SaveWorld();
+        }
     }
 
     private void GenerateGrid(int worldSizeX,int worldSizeZ)
@@ -32,53 +58,29 @@ public class ProceduralGeneration : MonoBehaviour
         {
             for (int l = 0; l < worldSizeZ; l++)
             {
-                //Spawn the blocks 
+                //Spawn the Cars 
                 Vector3 pos = new Vector3(gameObject.transform.position.x + (i * GridSpacing), 0, gameObject.transform.position.z + (l * GridSpacing));
-                GameObject Grid = Instantiate(Block, pos, Quaternion.identity);
-                //Random gen the object placement (scene)
-                int RandomObject = Random.Range(0, PlaceableSceneObjects.Length);
-                float RandomPlacement = Random.Range(0, 99);
-                float PlacementChance = RandomPlacement * ObjectDensity;
-
-                if (PlacementChance >= 70 && !JustGenerated)
+                NavMeshHit hit;
+                if (NavMesh.SamplePosition(pos, out hit, 0.2f, NavMesh.AllAreas))
                 {
-                    NavMeshHit hit;
-                    if(NavMesh.SamplePosition(pos,out hit,0.2f,NavMesh.AllAreas))
+                    if (JustGenerated && Random.Range(0, 100) > CarChance)
                     {
-                        JustGenerated = true;
-                        float RandomY = Random.Range(0, 360);
-                        GameObject SceneObj = Instantiate(PlaceableSceneObjects[RandomObject], Grid.transform.position, Quaternion.Euler(0, RandomY, 0));
-                        
+                        GameObject gridblock = Instantiate(PlaceableSceneObjects[Random.Range(0, 2)], pos, Quaternion.Euler(0,Random.Range(0,360),0));
+                        spawnedBlocks.Add(gridblock);
+                        JustGenerated = false;
                     }
-                    
-                   
+                    // Spawn the item pickups
+                    if (Random.Range(0, 100) < 30)
+                    {
+                       GameObject Pickup = Instantiate(PlaceablePickups[Random.Range(0, PlaceablePickups.Length)],pos, Quaternion.Euler(0, Random.Range(0, 360), 0));
+                        spawnedObjects.Add(Pickup);
+                    }
                 }
-                else
-                { 
-                    JustGenerated = false;
-                    GeneratePickup(Grid);
-                }
-                
-;            }
+                else 
+                JustGenerated = true;
+            }
 
         }
-    }
-    private void GeneratePickup(GameObject grid)
-    {
-        int RandomPickup = Random.Range(0, PlaceablePickups.Length);
-        int RandomPickupPlacement = Random.Range(0, 99);
-        float PickupChance = RandomPickupPlacement * PickupDensity;
-        if (PickupChance >= 70)
-        {
-            NavMeshHit hit;
-            if (NavMesh.SamplePosition(grid.transform.position, out hit, 1, NavMesh.AllAreas))
-            {
-                float RandomY = Random.Range(0, 360);
-                GameObject scenePickup = Instantiate(PlaceablePickups[RandomPickup], grid.transform.position + new Vector3(0, 5, 0), Quaternion.Euler(0, RandomY, 0));
-            }
-                
-        }
-        
     }
     private void GenerateNavmesh()
     {
@@ -87,5 +89,75 @@ public class ProceduralGeneration : MonoBehaviour
             NavmeshSurface[i].BuildNavMesh();
         }
     }
-    //private void GeneratePoint(GameObject grid,GameObject Point)
+
+    public void SaveWorld()
+    {
+        WorldData worldData = new WorldData();
+        //Cars Save 
+        foreach (GameObject block in spawnedBlocks)
+        {
+            worldData.BlockPositions.Add(block.transform.position);
+            worldData.ObjectRotation.Add(block.transform.rotation);
+            worldData.ObjectType.Add(block.name);
+        }
+        //Objects Save 
+        foreach (GameObject Pickup in spawnedObjects)
+        {
+            worldData.BlockPositions.Add(Pickup.transform.position);
+            worldData.ObjectRotation.Add(Pickup.transform.rotation);
+            worldData.ObjectType.Add(Pickup.name);
+        }
+        // Write that to a json file, i love data readablility !!!!
+        string json = JsonUtility.ToJson(worldData, true);
+        File.WriteAllText(saveFilePath, json);
+        Debug.Log("Saving world to: " + saveFilePath);
+    }
+
+    public void LoadWorld()
+    {
+        if (!File.Exists(saveFilePath))
+        {
+            Debug.LogError("No Save file");
+            return;
+        }
+
+        // Take the data from the file 
+        string json = File.ReadAllText(saveFilePath);
+        WorldData worldData = JsonUtility.FromJson<WorldData>(json);
+
+        // Now Spawn!  
+        for (int i = 0; i < worldData.BlockPositions.Count; i++)
+        {
+            Debug.Log(worldData.ObjectType[i]);
+            Debug.Log(worldData.BlockPositions[i]);
+            Debug.Log(worldData.ObjectRotation[i]);
+
+            GameObject SpawnedObject = Instantiate(ReturnPrefab(worldData.ObjectType[i]), worldData.BlockPositions[i], worldData.ObjectRotation[i]);
+        }
+
+
+        GenerateNavmesh();
+        Debug.Log("World loaded successfully!");
+    }
+
+    public GameObject ReturnPrefab(string Name)
+    {
+        string NewName = Name.Replace("(Clone)", "");
+        Debug.Log(NewName);
+        foreach (GameObject Car in PlaceableSceneObjects)
+        {
+            if (Car.name == NewName)
+            {
+                return Car;
+            }
+        }
+        foreach (GameObject Pickup in PlaceablePickups)
+        {
+            if (Pickup.name == NewName)
+            {
+                return Pickup;
+            }
+        }
+        return null;
+    }
 }
